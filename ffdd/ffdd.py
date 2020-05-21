5ca3197b367082d1d3395a6636aafca3982ea85f
Nick Machnik <nick.machnik@gmail.com>
2020-05-21 21:23:31 +0200

    Remove transcription function

--- ffdd/ffdd.py
+++ ffdd/ffdd.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import kb
-
 
 def truncate(seq: str):
     """Removes the prefix before the first ATG.
@@ -18,27 +16,3 @@ def truncate(seq: str):
         codon = seq[start:start+3]
         if codon == "ATG":
             return seq[start:]
-
-
-def transcribe(seq: str):
-    """Transcribe a DNA sequence into a protein.
-
-    Args:
-        seq (str): DNA sequence
-
-    Returns:
-        str: Protein sequence
-    """
-    seq = seq.upper()
-    translated = ""
-    for start in range(0, len(seq) - 2, 3):
-        codon = seq[start:start+3]
-        try:
-            aa = kb.codon_table[codon]
-        except KeyError:
-            print("Codon {} is not in the codon table!".format(codon))
-            exit(1)
-        if aa == '*':
-            return seq
-        else:
-            translated += aa
