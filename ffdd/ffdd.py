370fd92a6cd979f98272ca01ad055dba1a0ef3ce
Nick Machnik <nick.machnik@gmail.com>
2020-05-22 17:39:25 +0200

    Implement extraction of four fold degenerate codons at conserved sites

--- ffdd/ffdd.py
+++ ffdd/ffdd.py
@@ -90,7 +90,23 @@ def _triplet(seq: str, i: int):
     return seq[start:start+3]
 
 
-def _get_aligned_ffds(a: str, b: str, table_a="Standard", table_b="Standard"):
+def _aligned_ffds(a: str, b: str, table_a="Standard", table_b="Standard"):
+    """Extracts the four-fold degenerate codons at conserved sites
+    from a protein sequence alignment of two coding DNA sequences.
+
+    Args:
+        a (str): codding DNA sequence a
+        b (str): coding DNA sequence b
+        table_a (str, optional): NCBI table name as used in Bio.Data.CodonTable
+        table_b (str, optional): NCBI table name as used in Bio.Data.CodonTable
+
+    Yields:
+        tuple: aligned four-fold degenerate codons in sequence a and b
+
+    Raises:
+        ValueError: if any of the input sequences do not contain
+                    an ATG start codon.
+    """
     proteins = []
     for s, table in zip([a, table_a], [b, table_b]):
         ts = _truncate(s)
@@ -99,6 +115,14 @@ def _get_aligned_ffds(a: str, b: str, table_a="Standard", table_b="Standard"):
         else:
             proteins.append(_translate(ts, table))
     alignment = _align(*proteins)
-    unaligned_a_start = alignment[2][0][0]
-    unaligned_b_start = alignment[2][1][0]
+    # shorten the input sequences to the aligned subsequences
+    a = a[alignment[2][0][0]*3:alignment[2][0][1]*3]
+    b = b[alignment[2][1][0]*3:alignment[2][1][1]*3]
+    degenerate_aa = set(
+        _x_fold_degenerate_from_codon_table(4, table_a)).intersection(
+            set(_x_fold_degenerate_from_codon_table(4, table_b)))
 
+    # iterate over the aligned sequences
+    for i, (ca, cb) in enumerate(zip(*alignment[0])):
+        if ca == cb and ca in degenerate_aa:
+            yield _triplet[a, i], _triplet[b, i]
