ba478dad932325fc1b67635e4e82088a171e0c21
Nick Machnik <nick.machnik@gmail.com>
2020-05-22 13:15:42 +0200

    Implement translation

--- ffdd/ffdd.py
+++ ffdd/ffdd.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+# from skbio.alignment import local_pairwise_align_ssw as aln
+from skbio.sequence import Protein
+# from skbio.alignment._pairwise import blosum50
 
-def truncate(seq: str):
+
+def _truncate(seq: str):
     """Removes the prefix before the first ATG.
 
     Args:
@@ -16,3 +22,19 @@ def truncate(seq: str):
         codon = seq[start:start+3]
         if codon == "ATG":
             return seq[start:]
+
+
+def _translate(seq: str, table="Standard"):
+    """Translates a given DNA sequence into a
+    protein sequence, using a specified codon table.
+
+    Args:
+        seq (str): DNA sequence. Expects the coding strand and
+                    a start with a leading ATG codon.
+        table (str, optional): NCBI ID string for a codon table
+                               as used in biopython.
+
+    Returns:
+        skbio.sequence.Protein: Protein object with the translated sequence.
+    """
+    return Protein(str(Seq(seq, IUPAC.unambiguous_dna).translate(table=table)))
