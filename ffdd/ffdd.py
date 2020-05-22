1b78186b16020d48604d6667f15294f811dabd76
Nick Machnik <nick.machnik@gmail.com>
2020-05-22 15:53:08 +0200

    Implement extraction of x-fold degenerate codons from ncbi codon tables

--- ffdd/ffdd.py
+++ ffdd/ffdd.py
@@ -2,6 +2,7 @@
 
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
+from Bio.Data.CodonTable import unambiguous_dna_by_name
 from skbio.alignment import local_pairwise_align_ssw
 from skbio.sequence import Protein
 from skbio.alignment._pairwise import blosum50
@@ -31,8 +32,7 @@ def _translate(seq: str, table="Standard"):
     Args:
         seq (str): DNA sequence. Expects the coding strand and
                     a start with a leading ATG codon.
-        table (str, optional): NCBI ID string for a codon table
-                               as used in biopython.
+        table (str, optional): NCBI table name as used in Bio.Data.CodonTable
 
     Returns:
         skbio.sequence.Protein: Protein object with the translated sequence.
@@ -51,6 +51,29 @@ def _align(a: Protein, b: Protein):
         skbio.alignment.TabularMSA: skbio alignment table
     """
     return local_pairwise_align_ssw(a, b, substitution_matrix=blosum50)
+
+
+def _x_fold_degenerate_from_codon_table(x: int, table: str):
+    """Extracts amino acids that are encoded by x
+    different codons from an NCBI codon table.
+
+    Args:
+        x (int): fold degeneration to filter by.
+        table (str, optional): NCBI table name as used in Bio.Data.CodonTable
+
+    Returns:
+        dict: amino acids (keys) and their x codons (values)
+    """
+    codon_table = unambiguous_dna_by_name[str].forward_table
+    reverse_table = {}
+    for codon, aa in codon_table.items():
+        reverse_table.setdefault(aa, [])
+        reverse_table[aa].append[codon]
+    x_fold_table = {}
+    for aa, codons in reverse_table.items():
+        if len(codons) == x:
+            x_fold_table[aa] = codons
+    return x_fold_table
 
 
 def _get_aligned_ffds(a: str, b: str, table_a="Standard", table_b="Standard"):
