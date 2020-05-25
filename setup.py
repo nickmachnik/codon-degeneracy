b1f50c7465b91fdc9f8ec52603b7d3394cb18a73
Nick Machnik <nick.machnik@gmail.com>
2020-05-25 20:45:25 +0200

    depoendencies and package description

--- setup.py
+++ setup.py
@@ -26,16 +26,18 @@ class CleanCommand(Command):
 setup(
     name='codon-degeneracy',
     version=__version__,
-    description="""
-        Routines for the extraction of degenerate sides and calculation of rates
-        of neutral substitutions from sequences and alignments.""",
+    description=(
+        "Routines for the extraction of degenerate sides and calculation",
+        "of rates of neutral substitutions from sequences and alignments."
+        ),
     # long_description_content_type="text/markdown",
-    url='https://github.com/nickmachnik/codon-degeneracy',
+    url='https://github.com/nickmachnik/codon-degeneracy.git',
     setup_requires=[
         'setuptools>=18.0',
     ],
     packages=find_packages(),
     install_requires=[
+        'numpy>=1.8.0',
         'scikit-bio',
         'biopython'
     ],
